006a38a4e7023ccfbdc35bd7f442b4cda1d78267
vclorenzo <vanz.lorenzo@whitecloak.com>
2024-08-28 14:41:34 +0800

    added changes for skill section

--- src/Sections/Intro/index.tsx
+++ src/Sections/Intro/index.tsx
@@ -1,49 +1,45 @@
 import React from 'react';
 import Typewriter from 'typewriter-effect';
 import {
-  StyledSubtitleText,
-  StyledTitleText,
+	StyledSubtitleText,
+	StyledTitleText,
 } from '../../components/Text/index.styled.ts';
 import { VideoBg } from './index.styled.ts';
 import Video from '../../assets/bokeh.mp4';
 import { StyledLightButton } from '../../components/Buttons/index.styled.ts';
 import {
-  StyledContentContainer,
-  MasterContainer,
-  StyledTextContainer,
+	StyledContentContainer,
+	MasterContainer,
+	StyledTextContainer,
 } from '../../components/Container/index.styled.ts';
 
 const Intro = () => {
-  return (
-    <MasterContainer>
-      <StyledContentContainer id='intro'>
-        <VideoBg autoPlay loop muted src={Video} typeof='video/mp4' />
-        <StyledTextContainer isCentered={true}>
-          <StyledTitleText>
-            <h1>Vanz Lorenzo</h1>
-          </StyledTitleText>
-          <StyledSubtitleText>I specialize in</StyledSubtitleText>
-          <StyledSubtitleText>
-            <Typewriter
-              options={{
-                strings: [
-                  'Frontend Development',
-                  'Backend Development',
-                  'Test Automation',
-                ],
-                cursor: '',
-                autoStart: true,
-                loop: true,
-              }}
-            />
-          </StyledSubtitleText>
-          <a href='#projects'>
-            <StyledLightButton>View Projects</StyledLightButton>
-          </a>
-        </StyledTextContainer>
-      </StyledContentContainer>
-    </MasterContainer>
-  );
+	return (
+		<MasterContainer>
+			<StyledContentContainer id="intro">
+				<VideoBg autoPlay loop muted src={Video} typeof="video/mp4" />
+				<StyledTextContainer isCentered={true}>
+					<StyledTitleText>
+						<h1>Vanz Lorenzo</h1>
+					</StyledTitleText>
+					<StyledSubtitleText>I specialize in</StyledSubtitleText>
+					<StyledSubtitleText>
+						<Typewriter
+							options={{
+								strings: ['Frontend Development', 'Backend Development'],
+								cursor: '',
+								autoStart: true,
+								loop: true,
+							}}
+						/>
+					</StyledSubtitleText>
+					<a href="#projects">
+						<StyledLightButton>View Projects</StyledLightButton>
+					</a>
+				</StyledTextContainer>
+			</StyledContentContainer>
+		</MasterContainer>
+	);
 };
 
 export default Intro;
--- src/Sections/Skills/index.tsx
+++ src/Sections/Skills/index.tsx
@@ -1,32 +1,29 @@
 import React, { useEffect } from 'react';
 import {
-  MasterContainer,
-  StyledSplitContainer,
-  StyledTextContainer,
+	MasterContainer,
+	StyledSplitContainer,
+	StyledTextContainer,
 } from '../../components/Container/index.styled.ts';
 import {
-  SkillsShowcaseContainer,
-  SkillsShowcaseItem,
-  StyledSkillItemContainer,
+	SkillsShowcaseContainer,
+	SkillsShowcaseItem,
+	StyledSkillItemContainer,
 } from './index.styled.ts';
 import {
-  SiPostman,
-  SiSelenium,
-  SiReact,
-  SiNextdotjs,
-  SiGatsby,
-  SiApollographql,
-  SiTypescript,
-  SiStyledcomponents,
-  SiMui,
-  SiBootstrap,
-  SiNodedotjs,
-  SiExpress,
-  SiNestjs,
-  SiMongodb,
-  SiPostgresql,
-  SiGraphql,
-  SiStrapi,
+	SiReact,
+	SiNextdotjs,
+	SiGatsby,
+	SiTypescript,
+	SiStyledcomponents,
+	SiMui,
+	SiNodedotjs,
+	SiExpress,
+	SiMongodb,
+	SiPostgresql,
+	SiGraphql,
+	SiStrapi,
+	SiRedux,
+	SiJavascript,
 } from 'react-icons/si';
 import { FaSass } from 'react-icons/fa';
 import { StyledMainText } from '../../components/Text/index.styled.ts';
@@ -35,235 +32,193 @@ import { useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
 const ContainerVariantL = {
-  hidden: { x: '-100%', opacity: 0 },
-  visible: {
-    x: 0,
-    opacity: 1,
+	hidden: { x: '-100%', opacity: 0 },
+	visible: {
+		x: 0,
+		opacity: 1,
 
-    transition: {
-      type: 'tween',
-      duration: 1,
-      delay: 1,
+		transition: {
+			type: 'tween',
+			duration: 1,
+			delay: 1,
 
-      ease: 'easeInOut',
-      staggerChildren: 0.1,
-      when: 'beforeChildren',
-    },
-  },
+			ease: 'easeInOut',
+			staggerChildren: 0.1,
+			when: 'beforeChildren',
+		},
+	},
 };
 const ContainerVariantR = {
-  hidden: { x: '100%', opacity: 0 },
-  visible: {
-    x: 0,
-    opacity: 1,
+	hidden: { x: '100%', opacity: 0 },
+	visible: {
+		x: 0,
+		opacity: 1,
 
-    transition: {
-      type: 'tween',
-      duration: 1,
-      delay: 1,
+		transition: {
+			type: 'tween',
+			duration: 1,
+			delay: 1,
 
-      ease: 'easeInOut',
-      staggerChildren: 0.1,
-      when: 'beforeChildren',
-    },
-  },
+			ease: 'easeInOut',
+			staggerChildren: 0.1,
+			when: 'beforeChildren',
+		},
+	},
 };
 
 const CardVariant = {
-  hidden: { y: '20%', opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 1,
+	hidden: { y: '20%', opacity: 0 },
+	visible: {
+		y: 0,
+		opacity: 1,
 
-    transition: {
-      duration: 0.5,
-      ease: 'easeOut',
-    },
-  },
+		transition: {
+			duration: 0.5,
+			ease: 'easeOut',
+		},
+	},
 };
 const TextVariant = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
+	hidden: { opacity: 0 },
+	visible: {
+		opacity: 1,
 
-    transition: {
-      delay: 0.5,
-      duration: 5,
-      ease: 'easeOut',
-    },
-  },
+		transition: {
+			delay: 0.5,
+			duration: 5,
+			ease: 'easeOut',
+		},
+	},
 };
 
 const hoverObj = {
-  scale: 1.1,
-  originY: 1,
-  originX: 1,
+	scale: 1.1,
+	originY: 1,
+	originX: 1,
 };
 
-const SkillsQA = ({ animation }) => {
-  return (
-    <StyledSplitContainer isFullScreen={true}>
-      <StyledSkillItemContainer>
-        <StyledTextContainer isCentered={true}>
-          <StyledMainText
-            variants={TextVariant}
-            initial='hidden'
-            animate={animation}
-          >
-            QA
-          </StyledMainText>
-        </StyledTextContainer>
-      </StyledSkillItemContainer>
-      <StyledSkillItemContainer
-        variants={ContainerVariantR}
-        initial='hidden'
-        animate={animation}
-      >
-        <SkillsShowcaseContainer>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiSelenium />
-            <p>Selenium</p>
-          </SkillsShowcaseItem>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiPostman />
-            <p>Postman</p>
-          </SkillsShowcaseItem>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <UFT width={70} height={70} />
-            <p>HP UFT</p>
-          </SkillsShowcaseItem>
-        </SkillsShowcaseContainer>
-      </StyledSkillItemContainer>
-    </StyledSplitContainer>
-  );
-};
 const SkillsDev = ({ animation }) => {
-  return (
-    <StyledSplitContainer isFullScreen={true} id='skills' isReversed={true}>
-      <StyledSkillItemContainer
-        variants={ContainerVariantL}
-        initial='hidden'
-        animate={animation}
-      >
-        <SkillsShowcaseContainer>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiReact />
-            <p>ReactJS</p>
-          </SkillsShowcaseItem>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiNextdotjs />
-            <p>NextJS</p>
-          </SkillsShowcaseItem>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiGatsby />
-            <p>GasbyJS</p>
-          </SkillsShowcaseItem>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiApollographql />
-            <p>Apollo</p>
-          </SkillsShowcaseItem>
-        </SkillsShowcaseContainer>
-        <SkillsShowcaseContainer>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiStyledcomponents />
-            <p>Styled Components</p>
-          </SkillsShowcaseItem>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <FaSass />
-            <p>Sass</p>
-          </SkillsShowcaseItem>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiMui />
-            <p>Material UI</p>
-          </SkillsShowcaseItem>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiBootstrap />
-            <p>Bootstrap</p>
-          </SkillsShowcaseItem>
-        </SkillsShowcaseContainer>
-        <SkillsShowcaseContainer>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiNodedotjs />
-            <p>NodeJS</p>
-          </SkillsShowcaseItem>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiTypescript />
-            <p>Typescript</p>
-          </SkillsShowcaseItem>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiExpress />
-            <p>ExpressJS</p>
-          </SkillsShowcaseItem>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiNestjs />
-            <p>NestJS</p>
-          </SkillsShowcaseItem>
-        </SkillsShowcaseContainer>
-        <SkillsShowcaseContainer>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiMongodb />
-            <p>MongoDB</p>
-          </SkillsShowcaseItem>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiPostgresql />
-            <p>Postgresql</p>
-          </SkillsShowcaseItem>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiGraphql />
-            <p>Graphql</p>
-          </SkillsShowcaseItem>
-          <SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
-            <SiStrapi />
-            <p>Strapi CMS</p>
-          </SkillsShowcaseItem>
-        </SkillsShowcaseContainer>
-      </StyledSkillItemContainer>
-      <StyledSkillItemContainer>
-        <StyledTextContainer isCentered={true}>
-          <StyledMainText
-            variants={TextVariant}
-            initial='hidden'
-            animate={animation}
-          >
-            DEV
-          </StyledMainText>
-        </StyledTextContainer>
-      </StyledSkillItemContainer>
-    </StyledSplitContainer>
-  );
+	return (
+		<StyledSplitContainer isFullScreen={true} id="skills" isReversed={false}>
+			<StyledSkillItemContainer>
+				<StyledTextContainer isCentered={true}>
+					<StyledMainText
+						variants={TextVariant}
+						initial="hidden"
+						animate={animation}
+					>
+						SKILLS
+					</StyledMainText>
+				</StyledTextContainer>
+			</StyledSkillItemContainer>
+			<StyledSkillItemContainer
+				variants={ContainerVariantR}
+				initial="hidden"
+				animate={animation}
+			>
+				<SkillsShowcaseContainer>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<SiJavascript />
+						<p>Javascript</p>
+					</SkillsShowcaseItem>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<SiTypescript />
+						<p>Typescript</p>
+					</SkillsShowcaseItem>
+				</SkillsShowcaseContainer>
+				<SkillsShowcaseContainer>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<SiReact />
+						<p>ReactJS</p>
+					</SkillsShowcaseItem>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<SiNextdotjs />
+						<p>NextJS</p>
+					</SkillsShowcaseItem>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<SiGatsby />
+						<p>GasbyJS</p>
+					</SkillsShowcaseItem>
+				</SkillsShowcaseContainer>
+				<SkillsShowcaseContainer>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<SiStyledcomponents />
+						<p>Styled Components</p>
+					</SkillsShowcaseItem>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<FaSass />
+						<p>Sass</p>
+					</SkillsShowcaseItem>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<SiMui />
+						<p>Material UI</p>
+					</SkillsShowcaseItem>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<SiRedux />
+						<p>Redux Toolkit</p>
+					</SkillsShowcaseItem>
+				</SkillsShowcaseContainer>
+				<SkillsShowcaseContainer>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<SiNodedotjs />
+						<p>NodeJS</p>
+					</SkillsShowcaseItem>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<SiExpress />
+						<p>ExpressJS</p>
+					</SkillsShowcaseItem>
+				</SkillsShowcaseContainer>
+				<SkillsShowcaseContainer>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<SiMongodb />
+						<p>MongoDB</p>
+					</SkillsShowcaseItem>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<SiPostgresql />
+						<p>Postgresql</p>
+					</SkillsShowcaseItem>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<SiGraphql />
+						<p>Graphql</p>
+					</SkillsShowcaseItem>
+					<SkillsShowcaseItem variants={CardVariant} whileHover={hoverObj}>
+						<SiStrapi />
+						<p>Strapi CMS</p>
+					</SkillsShowcaseItem>
+				</SkillsShowcaseContainer>
+			</StyledSkillItemContainer>
+		</StyledSplitContainer>
+	);
 };
 
 const Skills = () => {
-  const { ref: refDev, inView: inViewDev } = useInView();
-  const { ref: refQA, inView: inViewQA } = useInView();
-  const animationOne = useAnimation();
-  const animationTwo = useAnimation();
+	const { ref: refDev, inView: inViewDev } = useInView();
+	const { ref: refQA, inView: inViewQA } = useInView();
+	const animationOne = useAnimation();
+	const animationTwo = useAnimation();
 
-  useEffect(() => {
-    if (inViewDev) {
-      animationOne.start('visible');
-    }
-    if (inViewQA) {
-      animationTwo.start('visible');
-    }
-    if (!inViewDev) {
-      animationOne.start('hidden');
-    }
-    if (!inViewQA) {
-      animationTwo.start('hidden');
-    }
-  }, [animationOne, animationTwo, inViewDev, inViewQA]);
+	useEffect(() => {
+		if (inViewDev) {
+			animationOne.start('visible');
+		}
+		if (inViewQA) {
+			animationTwo.start('visible');
+		}
+		if (!inViewDev) {
+			animationOne.start('hidden');
+		}
+		if (!inViewQA) {
+			animationTwo.start('hidden');
+		}
+	}, [animationOne, animationTwo, inViewDev, inViewQA]);
 
-  return (
-    <MasterContainer bgColor={'#333'}>
-      <div ref={refDev}>
-        <SkillsDev animation={animationOne} />
-      </div>
-      <div ref={refQA}>
-        <SkillsQA animation={animationTwo} />
-      </div>
-    </MasterContainer>
-  );
+	return (
+		<MasterContainer bgColor={'#333'}>
+			<div ref={refDev}>
+				<SkillsDev animation={animationOne} />
+			</div>
+		</MasterContainer>
+	);
 };
 
 export default Skills;
